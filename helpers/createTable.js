72690468edfaf8a38e099a19973f82e8f3661dd3
migger <i.mihailyuk@cian.ru>
2020-12-23 00:48:33 +0300

    added create table

--- helpers/createTable.js
+++ helpers/createTable.js
@@ -2,16 +2,32 @@ const ydb = require('./ydb')
 
 new Promise(async (resolve) => {
     await ydb.query('CreateTable', {
-            TableName: 'll-auth/users',
+            TableName: 'll-auth/keys',
             AttributeDefinitions: [
-                {AttributeName: "userId", AttributeType: "N"},
-                {AttributeName: "userName", AttributeType: "S"},
-                {AttributeName: "passwordHash", AttributeType: "S"},
+                {AttributeName: "id", AttributeType: "N"},
+                {AttributeName: "privateKey", AttributeType: "S"},
+                {AttributeName: "publicKey", AttributeType: "S"},
+                {AttributeName: "expireAt", AttributeType: "N"},
             ],
             KeySchema: [
-                {AttributeName: 'userId', KeyType: "HASH"}
+                {AttributeName: 'id', KeyType: "HASH"}
             ]
         }
     ).then(console.log);
 });
 
+// new Promise(async (resolve) => {
+//     await ydb.query('CreateTable', {
+//             TableName: 'll-auth/users',
+//             AttributeDefinitions: [
+//                 {AttributeName: "userId", AttributeType: "N"},
+//                 {AttributeName: "userName", AttributeType: "S"},
+//                 {AttributeName: "passwordHash", AttributeType: "S"},
+//             ],
+//             KeySchema: [
+//                 {AttributeName: 'userId', KeyType: "HASH"}
+//             ]
+//         }
+//     ).then(console.log);
+// });
+//
--- helpers/ydb.js
+++ helpers/ydb.js
@@ -2,6 +2,6 @@ const YandexDB = require('../index.js')
 
 module.exports = new YandexDB({
     endpoint: 'https://docapi.serverless.yandexcloud.net/ru-central1/b1glho6pdsm66b2a60a2/etn03f72tb81nr1og6hr',
-    keyId: 'ch_jywbetqXPW5lrBhB_',
-    secretKey: '_n1L15b1zNoVXqvbT9R8PrSrAHxNo8lEqlHGIncx',
+    keyId: '',
+    secretKey: '',
 });
